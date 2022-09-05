3a3554766b3115daec4a7ca1c82f98a0ce74437d
EC2 Default User <ec2-user@ip-172-31-11-38.us-west-2.compute.internal>
2022-09-05 16:23:00 +0000

    API Dynamo GetAll Access Pattern

--- api.py
+++ api.py
@@ -6,8 +6,6 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 client = boto3.client('dynamodb',
-  aws_access_key_id='yyyy',
-  aws_secret_access_key='xxxx',
   region_name='us-east-1')
 
 @app.route('/test', methods=['GET'])
@@ -18,40 +16,40 @@ def test_route():
 @app.route('/getAll', methods=['GET'])
 def query_unicorns():
     dynamo = boto3.resource('dynamodb', region_name='us-west-2')
-    table = dynamo.Table('Posts')
+    table = dynamo.Table(client.listTables()['TableNames'][0])
     response = table.scan()
     data = response['Items']
     return json.dumps(data)
     
 
-@app.route('/', methods=['PUT'])
-def create_post():
-    record = json.loads(request.data)
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-    if not data:
-        records = [record]
-    else:
-        records = json.loads(data)
-        records.append(record)
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(records, indent=2))
-    return jsonify(record)
+# @app.route('/', methods=['PUT'])
+# def create_post():
+#     record = json.loads(request.data)
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#     if not data:
+#         records = [record]
+#     else:
+#         records = json.loads(data)
+#         records.append(record)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(records, indent=2))
+#     return jsonify(record)
 
-@app.route('/', methods=['POST'])
-def update_post():
-    record = json.loads(request.data)
-    new_records = []
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-    for r in records:
-        if r['name'] == record['name']:
-            r['email'] = record['email']
-        new_records.append(r)
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(new_records, indent=2))
-    return jsonify(record)
+# @app.route('/', methods=['POST'])
+# def update_post():
+#     record = json.loads(request.data)
+#     new_records = []
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#         records = json.loads(data)
+#     for r in records:
+#         if r['name'] == record['name']:
+#             r['email'] = record['email']
+#         new_records.append(r)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(new_records, indent=2))
+#     return jsonify(record)
     
 # @app.route('/', methods=['DELETE'])
 # def delte_record():

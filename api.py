57790e1eaeb2258e476f02a0a6bc0441d65142ad
EC2 Default User <ec2-user@ip-172-31-11-38.us-west-2.compute.internal>
2022-09-06 20:38:20 +0000

    Adding Replicas

--- api.py
+++ api.py
@@ -22,49 +22,5 @@ def query_unicorns():
     return json.dumps(data)
     
 
-# @app.route('/', methods=['PUT'])
-# def create_post():
-#     record = json.loads(request.data)
-#     with open('/tmp/data.txt', 'r') as f:
-#         data = f.read()
-#     if not data:
-#         records = [record]
-#     else:
-#         records = json.loads(data)
-#         records.append(record)
-#     with open('/tmp/data.txt', 'w') as f:
-#         f.write(json.dumps(records, indent=2))
-#     return jsonify(record)
-
-# @app.route('/', methods=['POST'])
-# def update_post():
-#     record = json.loads(request.data)
-#     new_records = []
-#     with open('/tmp/data.txt', 'r') as f:
-#         data = f.read()
-#         records = json.loads(data)
-#     for r in records:
-#         if r['name'] == record['name']:
-#             r['email'] = record['email']
-#         new_records.append(r)
-#     with open('/tmp/data.txt', 'w') as f:
-#         f.write(json.dumps(new_records, indent=2))
-#     return jsonify(record)
-    
-# @app.route('/', methods=['DELETE'])
-# def delte_record():
-#     record = json.loads(request.data)
-#     new_records = []
-#     with open('/tmp/data.txt', 'r') as f:
-#         data = f.read()
-#         records = json.loads(data)
-#         for r in records:
-#             if r['name'] == record['name']:
-#                 continue
-#             new_records.append(r)
-#     with open('/tmp/data.txt', 'w') as f:
-#         f.write(json.dumps(new_records, indent=2))
-#     return jsonify(record)
-
 if __name__ == '__main__':
     app.run(port=80, host="0.0.0.0", debug=True)
